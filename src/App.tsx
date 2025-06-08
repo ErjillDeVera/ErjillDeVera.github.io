074060a8d469aecdf16e07cb7215bd2eea2146e1
Erjill <erjilldevera@icloud.com>
2025-06-08 23:35:30 +0100

    Refactored sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Banner from "./components/Banner";
-import About from "./components/About";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
-      <About />
     </div>
   );
 }
--- src/components/About.tsx
+++
@@ -1,8 +0,0 @@
-import { Row, Col, Container } from "react-bootstrap";
-import profilePic from "../assets/images/Profilepic.png";
-
-function About() {
-  return <section className="about" id="about"></section>;
-}
-
-export default About;
